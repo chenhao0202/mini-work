2d5b9cdc6f889d7a9456a7fdb882d9e3349d3cfa
mini <chenhao0202@163.com>
2018-06-30 23:04:15 +0800

    git commit

---
+++ include/public.h
@@ -0,0 +1,8 @@
+#ifndef __PUBLIC_H__
+#define __PUBLIC_H__
+#endif
+
+
+#include <iostream>
+
+using namespace std;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-
-using namespace std;
+#include "public.h"
 
 class animal
 {
